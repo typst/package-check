3a4fc4dc31bc34e86cde3df66268594443026986
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-04-13 15:58:44 +0200

    Print fatal error to stdout

--- src/cli.rs
+++ src/cli.rs
@@ -38,7 +38,7 @@ pub async fn main(spec_or_path: String, json_output: bool) {
             }
         }
         Err(e) => {
-            eprintln!("Fatal error: {}", e.message);
+            println!("Fatal error: {}", e.message);
             exit(1)
         }
     }
